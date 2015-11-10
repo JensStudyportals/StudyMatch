3afb649f4340c60dadf94603fb43299bce0ae6ed
Steve Zeeuwe <steve@studyportals.com>
2015-11-10 16:28:54 +0100

    Add handlebars template.

--- js/frontend.js
+++ js/frontend.js
@@ -9,5 +9,21 @@ $(document).ready(function() {
 		var $countryList = $('#allCountries');
 
 		$countryList.toggle('slow');
+
+		if($showMoreButton.text() == 'Show all cities'){
+			$(this).text('Hide all cities');
+		} else {
+			$(this).text('Show all cities');
+		}
 	});
+
+	var disciplineSource = $("#disciplines-template").html();
+	var template = Handlebars.compile(disciplineSource);
+
+	var disciplineContext = {title: "test",
+		disciplines: [{name:"test1"},{name:"test2"}, {name:"test3"}]};
+	var discHtml = template(disciplineContext);
+
+	$('#disciplines-placeholder').html( discHtml);
+
 });
